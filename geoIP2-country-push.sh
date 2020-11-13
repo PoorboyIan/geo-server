4b2a51c2644fb1c2c3916f45c2bb7c314f93d407
PoorboyIan <1361005605@qq.com>
2020-11-13 16:17:13 +0800

    update db

--- geoIP2-country-push.sh
+++ geoIP2-country-push.sh
@@ -2,4 +2,10 @@
 
 path=~/go/src/geo-server/
 
-pwd
+gitpath = "~/go/src/geo-server/"
+git rm src/main/resources/db/GeoIP2-Country.mmdb
+mv ~/GeoIP2-Country.mmdb ~/go/src/geo-server/src/main/resources/db/
+git add .
+git commit -m "update db"
+git push origin master
+

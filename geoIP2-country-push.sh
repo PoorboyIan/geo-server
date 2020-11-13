6f60883b992ae923fe0f9d8d110b4ba77af94e94
PoorboyIan <1361005605@qq.com>
2020-11-13 16:08:17 +0800

    update db

--- geoIP2-country-push.sh
+++ geoIP2-country-push.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! /bin/bash
 
 path=~/go/src/geo-server/
 
--- main.go
+++ main.go
@@ -1,7 +1,30 @@
-package geo_server
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/oschwald/geoip2-golang"
+	"log"
+	"net"
+)
 
 func main(){
-	fmt.Println("hello")
+	db, err := geoip2.Open("src/main/resources/db/GeoIP2-Country.mmdb")
+	if err != nil{
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	ip := net.ParseIP("10.200.50.34")
+	record, err := db.City(ip)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Portuguese (BR) city name: %v\n", record.City.Names["pt-BR"])
+	if len(record.Subdivisions) > 0 {
+		fmt.Printf("English subdivision name: %v\n", record.Subdivisions[0].Names["en"])
+	}
+	fmt.Printf("Russian country name: %v\n", record.Country.Names["ru"])
+	fmt.Printf("ISO country code: %v\n", record.Country.IsoCode)
+	fmt.Printf("Time zone: %v\n", record.Location.TimeZone)
+	fmt.Printf("Coordinates: %v, %v\n", record.Location.Latitude, record.Location.Longitude)
 }
